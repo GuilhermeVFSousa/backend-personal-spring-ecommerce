ce8e9c8fc5e759a490aae43dd3f5cf0f664c69e6
GuilhermeVFSousa <guilhermevfsousa@gmail.com>
2023-01-11 12:55:25 -0300

    Ajusto CrossOrigin para ProdutoRepository e ProdutorCategoriaRepository

--- src/main/java/com/gvfs/ecommerce/repositories/ProdutoRepository.java
+++ src/main/java/com/gvfs/ecommerce/repositories/ProdutoRepository.java
@@ -2,10 +2,12 @@ package com.gvfs.ecommerce.repositories;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+import org.springframework.web.bind.annotation.CrossOrigin;
 
 import com.gvfs.ecommerce.entity.Produto;
 
-@RepositoryRestResource(collectionResourceRel = "produto", path = "produtos")
+@CrossOrigin("http://localhost:4200")
+@RepositoryRestResource(collectionResourceRel = "produtos", path = "produtos")
 public interface ProdutoRepository extends JpaRepository<Produto, Long> {
 
 }
