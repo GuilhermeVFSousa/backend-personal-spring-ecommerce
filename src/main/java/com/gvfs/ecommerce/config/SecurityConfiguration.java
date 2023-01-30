d19c772ae9942a5d02cbb71c63a32b88fca89ac6
GuilhermeVFSousa <guilhermevfsousa@gmail.com>
2023-01-30 15:13:45 -0300

    Protegendo os Endpoints na classe de configuração do Spring Security

---
+++ src/main/java/com/gvfs/ecommerce/config/SecurityConfiguration.java
@@ -0,0 +1,27 @@
+package com.gvfs.ecommerce.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+public class SecurityConfiguration {
+	
+	@Bean
+	public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+		
+		// protegendo o endpoint /api/orders
+		http.authorizeRequests(configurer -> configurer
+				.antMatchers("/api/orders**")
+				.authenticated())
+		.oauth2ResourceServer()
+		.jwt();
+		
+		// add CORS filter
+		http.cors();
+		
+		return http.build();
+	}
+
+}
