f79241a81626df601b8dd31762b34d652205ce5b
GuilhermeVFSousa <guilhermevfsousa@gmail.com>
2023-01-31 21:59:47 -0300

    CheckoutController - implementado o endpoint para /payment-intent

--- src/main/java/com/gvfs/ecommerce/controller/CheckoutController.java
+++ src/main/java/com/gvfs/ecommerce/controller/CheckoutController.java
@@ -1,13 +1,18 @@
 package com.gvfs.ecommerce.controller;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.gvfs.ecommerce.dto.PaymentInfo;
 import com.gvfs.ecommerce.dto.Purchase;
 import com.gvfs.ecommerce.dto.PurchaseResponse;
 import com.gvfs.ecommerce.service.CheckoutService;
+import com.stripe.exception.StripeException;
+import com.stripe.model.PaymentIntent;
 
 @RestController
 @RequestMapping("/api/checkout")
@@ -24,5 +29,15 @@ public class CheckoutController {
 		PurchaseResponse purchaseResponse = checkoutService.placeOrder(purchase);
 		
 		return purchaseResponse;
+	}
+	
+	@PostMapping("/payment-intent")
+	public ResponseEntity<String> createPaymentIntent(@RequestBody PaymentInfo paymentInfo) throws StripeException {
+		
+		PaymentIntent paymentIntent = checkoutService.createPaymentIntent(paymentInfo);
+		
+		String paymentStr = paymentIntent.toJson();
+		
+		return new ResponseEntity<>(paymentStr, HttpStatus.OK);
 	}
 }
