92ed4b5d0c35313b392b1f16acf1455d8f1273b3
GuilhermeVFSousa <guilhermevfsousa@gmail.com>
2023-01-31 21:52:12 -0300

    Checkout Service - Implementado o método createPaymentIntent do Stripe

--- src/main/java/com/gvfs/ecommerce/service/CheckoutService.java
+++ src/main/java/com/gvfs/ecommerce/service/CheckoutService.java
@@ -1,10 +1,15 @@
 package com.gvfs.ecommerce.service;
 
+import com.gvfs.ecommerce.dto.PaymentInfo;
 import com.gvfs.ecommerce.dto.Purchase;
 import com.gvfs.ecommerce.dto.PurchaseResponse;
+import com.stripe.exception.StripeException;
+import com.stripe.model.PaymentIntent;
 
 public interface CheckoutService {
 	
 	PurchaseResponse placeOrder(Purchase purchase);
+	
+	PaymentIntent createPaymentIntent(PaymentInfo paymentInfo) throws StripeException;
 
 }
--- src/main/java/com/gvfs/ecommerce/service/CheckoutServiceImpl.java
+++ src/main/java/com/gvfs/ecommerce/service/CheckoutServiceImpl.java
@@ -1,26 +1,38 @@
 package com.gvfs.ecommerce.service;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 import java.util.Set;
 import java.util.UUID;
 
 import javax.transaction.Transactional;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
+import com.gvfs.ecommerce.dto.PaymentInfo;
 import com.gvfs.ecommerce.dto.Purchase;
 import com.gvfs.ecommerce.dto.PurchaseResponse;
 import com.gvfs.ecommerce.entity.Customer;
 import com.gvfs.ecommerce.entity.Order;
 import com.gvfs.ecommerce.entity.OrderItem;
 import com.gvfs.ecommerce.repositories.CustomerRepository;
+import com.stripe.Stripe;
+import com.stripe.exception.StripeException;
+import com.stripe.model.PaymentIntent;
 
 @Service
 public class CheckoutServiceImpl implements CheckoutService {
 	
 	private CustomerRepository customerRepository;
 	
-	public CheckoutServiceImpl(CustomerRepository customerRepository) {
+	public CheckoutServiceImpl(CustomerRepository customerRepository, @Value("${stripe.key.secret}") String secretKey) {
 		this.customerRepository = customerRepository;
+		
+		// inicializando Stripe API com secret Key
+		Stripe.apiKey = secretKey;
 	}
 
 	@Override
@@ -69,6 +81,20 @@ public class CheckoutServiceImpl implements CheckoutService {
 		
 		// gerar um UUID aleatório (UUID version-4)
 		return UUID.randomUUID().toString();
+	}
+
+	@Override
+	public PaymentIntent createPaymentIntent(PaymentInfo paymentInfo) throws StripeException {
+		
+		List<String> paymentMethodTypes = new ArrayList<>();
+		paymentMethodTypes.add("card");
+		
+		Map<String, Object> params = new HashMap<>();
+		params.put("amount", paymentInfo.getAmount());
+		params.put("currency", paymentInfo.getCurrency());
+		params.put("payment_method_types", paymentMethodTypes);
+		
+		return PaymentIntent.create(params);
 	}
 	
 
